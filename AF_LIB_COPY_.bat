71aa417d9da00815960a56c518b91d25d29ad09b
finder-zhang <644832501@qq.com>
2014-08-15 16:00:43 +0800

    增加了批处理复制文件，LIB生成目录已经改成工程相对目录

---
+++ AF_LIB_COPY_.bat
@@ -0,0 +1,18 @@
+rem if not exist "D:\Program Files (x86)\IAR Systems\Embedded Workbench 6.0\8051\AfLib_F\lib" (
+rem md "D:\Program Files (x86)\IAR Systems\Embedded Workbench 6.0\8051\AfLib_F\lib")
+
+rem if not exist "D:\Program Files (x86)\IAR Systems\Embedded Workbench 6.0\8051\AfLib_F\inc" (
+rem md "D:\Program Files (x86)\IAR Systems\Embedded Workbench 6.0\8051\AfLib_F\inc")
+
+rem copy ".\Debug\Exe\AfLib_F.lib" "D:\Program Files (x86)\IAR Systems\Embedded Workbench 6.0\8051\AfLib_F\lib\"
+
+rem copy ".\AfSrc\*.h"             "D:\Program Files (x86)\IAR Systems\Embedded Workbench 6.0\8051\AfLib_F\inc\"
+
+set AFLIB_PATH=%IAR_EW8051_DIR%\AfLib_F\lib
+set AFINC_PATH=%IAR_EW8051_DIR%\AfLib_F\inc
+
+if not exist "%AFLIB_PATH%" md "%AFLIB_PATH%"
+if not exist "%AFINC_PATH%" md "%AFINC_PATH%"
+
+copy ".\Debug\Exe\AfLib_F.lib" "%AFLIB_PATH%"
+copy ".\AfSrc\*.h"             "%AFINC_PATH%"
